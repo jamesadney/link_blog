eb71f253d427824ba111efdcf59d2bacaa49a9c0
James Adney <jfadney@gmail.com>
2013-05-09 21:39:15 -0700

    setup with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = 'ee0551c01923184b7f65b0e08e5130299968d5bcb502afbcdd8a8045a115ec183072c9eb546ed8b2d118bd5473a8f81291efc8a876bc099bca7fdda5bffa6390'
